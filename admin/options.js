583066d78c5a5567025d5f1c255dffca79d9d47d
hugurp <hugurp@pm.me>
2020-12-15 17:40:23 +0100

    update content/ folder structure to pods for each company

--- admin/options.js
+++ admin/options.js
@@ -2,18 +2,17 @@ import collections from './collections.js'
 
 export default {
     config: {
-	// Skips config.yml.
-	// By not skipping, the configs will be merged, with the js-version taking priority.
 	load_config_file: false,
 	display_url: window.location.origin,
 
 	backend: {
 	    name: 'github',
 	    repo: 'joblistcity/companies',
-	    open_authoring: true
+	    open_authoring: true,
 	},
+	publish_mode: 'editorial_workflow',
 
-	media_folder: 'static/media/uploads',
+	media_folder: 'media/uploads',
 	public_folder: 'media/uploads',
 
 	collections
