54216d5f4ca26cb1636ac032feed529918cdee68
hugurp <hugurp@pm.me>
2020-12-16 15:00:00 +0100

    improve landing page

--- edit/fields/jobBoardProvider.js
+++ edit/fields/jobBoardProvider.js
@@ -2,6 +2,6 @@ export default {
     label: 'Job board Provider',
     name: 'job_board_provider',
     widget: 'string',
-    hint: 'The name/id of this job board provider; ex: personio, smartrecruiters etc.',
+    hint: 'The name/id of this job board provider; Can be pne of: ["greenhouse", "recruitee", "smartrecruiters"]. Check out https://gitlab.com/joblist/workers to learn more about how to implement a new job board provider.',
     required: false
 }
